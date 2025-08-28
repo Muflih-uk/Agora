155a6b530bd48551a7507995790f1e9cf3d66989
Muflih-uk <muhammedmuflih875@gmail.com>
2025-08-28 20:00:56 +0530

    add the end-points categories

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
 from .models import EventModel
 from .serializers import EventSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+
 
 class EventViewSet(viewsets.ModelViewSet):
 
@@ -14,3 +17,9 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(host=self.request.user)
+
+    @action(detail=False, methods=['get'], url_path='category/(?P<category_name>[^/.]+)')
+    def get_by_category(self, request, category_name=None):
+        events = self.get_queryset().filter(category__iexact=category_name)
+        serializer = self.get_serializer(events, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
