8c7ed303d342ed8adadead8c12fe8685571f4504
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-25 20:57:02 +0530

    User Model Updated

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
-# That is App User Model
 class AppUser(AbstractUser):
     username = models.CharField(max_length=50, unique= True)
     email = models.EmailField(_('email address'),unique= True)
